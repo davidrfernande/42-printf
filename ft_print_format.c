459159076d03dfb3c9c179ad99fd65ea87eaf894
davidrfernande <david.fernandes.2k3@gmail.com>
2023-11-10 17:43:38 +0000

    10/11 18

--- ft_print_format.c
+++ ft_print_format.c
@@ -6,7 +6,7 @@
 /*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 14:45:29 by davidro2          #+#    #+#             */
-/*   Updated: 2023/11/09 15:49:44 by david            ###   ########.fr       */
+/*   Updated: 2023/11/10 17:43:09 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,19 @@ char    *ft_print_format(char c)
     if (c == 'c')
         ft_putchar(c);
     if (c == 's')
-        ft_putstr()
+        ft_putstr();
+    if (c == 'p')
+        ft_
+    if (c == 'd')
+        ft_lputnbr();
+    if (C == 'i')
+        ft_lputnbr();
+    if (c == 'u')
+        ft_lputnbr();
+    if (c == 'x')
+        ft_
+    if (c == 'X')
+        ft_
+    if (c == '%')
+        ft_
 }
