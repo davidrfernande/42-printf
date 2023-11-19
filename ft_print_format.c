5bb6403681c7e89e2d0540a19f646e70b98da8ab
davidrfernande <david.fernandes.2k3@gmail.com>
2023-11-19 23:38:07 +0000

    19/11 02

--- ft_print_format.c
+++ ft_print_format.c
@@ -6,7 +6,7 @@
 /*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 16:15:25 by david             #+#    #+#             */
-/*   Updated: 2023/11/19 19:40:10 by david            ###   ########.fr       */
+/*   Updated: 2023/11/19 23:33:43 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int	ft_print_format(char c, va_list arg, int counter)
 		return (counter += ft_putchar_pf(va_arg(arg, int)));
 	else if (c == 's')
 		return (counter += ft_putstr_pf(va_arg(arg, char *)));
+	else  if (c == 'p')
+		return (counter += ft_vdtohex_pf(va_arg(arg, void *)));
 	else if (c == 'd' || c == 'i' )
 		return (counter += ft_putnbr_pf(va_arg(arg, int)));
 	else if (c == 'u')
