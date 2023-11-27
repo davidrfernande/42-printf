a1ab00801ff6bcbb847587f221e1a41b67285bb6
David Rodrigues Fernandes <davidro2@c1r7s1.42lisboa.com>
2023-11-27 14:01:26 +0000

    27/11 14

--- ft_vdtohex_pf.c
+++ ft_vdtohex_pf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_vdtohex_pf.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
+/*   By: davidro2 <davidro2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 16:49:35 by davidro2          #+#    #+#             */
-/*   Updated: 2023/11/21 23:17:09 by david            ###   ########.fr       */
+/*   Updated: 2023/11/27 13:58:13 by davidro2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_vdtohex_pf(unsigned long int ptr)
 		return (counter);
 	}
 	counter += write(1, "0x", 2);
-	counter += ft_puthex_pf(ptr, 'X');
+	counter += ft_puthex_pf(ptr, 'x');
 	return (counter);
 }
 
