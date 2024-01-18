423b70f614262735bf5bdc8e0148575f5c7ecc90
David Rodrigues Fernandes <davidro2@c1r5s6.42lisboa.com>
2024-01-18 14:41:40 +0000

    18/01 14

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "ft_printf.h"
+
+int main()
+{
+    ft_printf("%s\n", "onepiece");
+    printf("%s\n", "onepiece");
+    ft_printf("%d\n", "420");
+    printf("%d\n", "420");
+    ft_printf("%x\n", "420");
+    printf("%x\n", "420");
+    ft_printf("%u\n", "420");
+    printf("%u\n", "420");
+    ft_printf("%p\n", "420");
+    printf("%p\n", "420");
+}
